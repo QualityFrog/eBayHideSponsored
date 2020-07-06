b292269acf8502b564f4e49ce5fa4b129dd61542
Ben Simo <ben@qualityfrog.com>
2020-07-06 01:41:52 -0700

    Added support for additional ebay sites
    Canada, UK, Germany, Austria, Australia, Spain

--- hideSponsored.js
+++ hideSponsored.js
@@ -1,4 +1,4 @@
 var si=document.getElementsByClassName('s-item');
 for(var x=0;x<si.length;x++)
- if(si[x].innerText.match("^SPONSORED"))
+ if(si[x].innerText.match("^(PATROCINADO|SPONSORISÉ|ANZEIGE|SPONSORED)\n"))
   si[x].style.display='none';
