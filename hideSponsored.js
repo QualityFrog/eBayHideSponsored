de6c2d396bb570390957ce1960699a3dbafecf6a
Ben Simo <ben@qualityfrog.com>
2020-08-29 14:03:34 -0700

    Delete hideSponsored.js

--- hideSponsored.js
+++
@@ -1,4 +0,0 @@
-var si=document.getElementsByClassName('s-item');
-for(var x=0;x<si.length;x++)
- if(si[x].innerText.match("^(Gesponsord|PATROCINADO|SPONSORISÉ|ANZEIGE|SPONSORED)\n"))
-  si[x].style.display='none';
